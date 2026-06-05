6ffdee3d2ac8b6877d6721b106b2b518c83a76c7
Brian Bakari <brianbakari22@gmail.com>
2026-06-05 11:42:46 +0400

    Fix item photo display in marketplace

--- src/components/customer/CustomerPartsMarketplace.jsx
+++ src/components/customer/CustomerPartsMarketplace.jsx
@@ -249,7 +249,7 @@ export default function CustomerPartsMarketplace() {
                 </button>
                 </div>
               </div>
-            )})
+            ))}
           </div>
         </>
       )}
