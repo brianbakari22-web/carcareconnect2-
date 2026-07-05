9a7c244186b9e874082904f6ae8d436f31d571db
Brian Bakari <brianbakari22@gmail.com>
2026-07-05 16:17:53 +0400

    Fix isMobile not defined in LandingPage - use fixed 120px logo height instead

---
+++ dist/assets/index-BskBzp89.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CO2xq3xw.js","assets/index-G-SDgEcF.js","assets/vendor-C1lte47L.js","assets/supabase-CwrFKnKJ.js","assets/ui-CaTqAy3Q.js","assets/index-C3tPJB2X.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-G-SDgEcF.js";import"./vendor-C1lte47L.js";import"./supabase-CwrFKnKJ.js";import"./ui-CaTqAy3Q.js";const m=r("App",{web:()=>t(()=>import("./web-CO2xq3xw.js"),__vite__mapDeps([0,1,2,3,4,5])).then(p=>new p.AppWeb)});export{m as App};
+//# sourceMappingURL=index-BskBzp89.js.map
