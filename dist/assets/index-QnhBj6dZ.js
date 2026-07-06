1e22e0d0b408daf789b0c4d30342288b8d9797e7
Brian Bakari <brianbakari22@gmail.com>
2026-07-06 20:31:20 +0400

    Upgrade service photo display in CustomerServices - full bleed image extends to card edges with gradient overlay, category badge overlaid on photo, photo count badge, cleaner 180px height with proper objectFit cover

---
+++ dist/assets/index-QnhBj6dZ.js
@@ -0,0 +1,3 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CCHe-R_O.js","assets/index-Bm85bxYG.js","assets/vendor-C1lte47L.js","assets/supabase-CwrFKnKJ.js","assets/ui-CaTqAy3Q.js","assets/index-C3tPJB2X.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as t}from"./index-Bm85bxYG.js";import"./vendor-C1lte47L.js";import"./supabase-CwrFKnKJ.js";import"./ui-CaTqAy3Q.js";const m=e("Share",{web:()=>t(()=>import("./web-CCHe-R_O.js"),__vite__mapDeps([0,1,2,3,4,5])).then(r=>new r.ShareWeb)});export{m as Share};
+//# sourceMappingURL=index-QnhBj6dZ.js.map
