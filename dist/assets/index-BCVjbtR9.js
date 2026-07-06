7a52d1711ceb9edf19ba818ee9065321cfd3c76f
Brian Bakari <brianbakari22@gmail.com>
2026-07-06 20:02:38 +0400

    Show service photos to customers - service cards in CustomerServices now display provider-uploaded photos at top of card, shows photo count badge if multiple photos, falls back gracefully when no photos uploaded

--- dist/assets/index-BCVjbtR9.js
+++
@@ -1,3 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CJLpBEfq.js","assets/index-B7D6M6AL.js","assets/vendor-C1lte47L.js","assets/supabase-CwrFKnKJ.js","assets/ui-CaTqAy3Q.js","assets/index-C3tPJB2X.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-B7D6M6AL.js";import"./vendor-C1lte47L.js";import"./supabase-CwrFKnKJ.js";import"./ui-CaTqAy3Q.js";const m=r("App",{web:()=>t(()=>import("./web-CJLpBEfq.js"),__vite__mapDeps([0,1,2,3,4,5])).then(p=>new p.AppWeb)});export{m as App};
-//# sourceMappingURL=index-BCVjbtR9.js.map
--- dist/assets/index-CWWlRcJT.js
+++
@@ -1,3 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BuI-snkz.js","assets/index-B7D6M6AL.js","assets/vendor-C1lte47L.js","assets/supabase-CwrFKnKJ.js","assets/ui-CaTqAy3Q.js","assets/index-C3tPJB2X.css"])))=>i.map(i=>d[i]);
-import{r as t,_ as r}from"./index-B7D6M6AL.js";import{f as e}from"./synapse-akrKn89j.js";import"./vendor-C1lte47L.js";import"./supabase-CwrFKnKJ.js";import"./ui-CaTqAy3Q.js";const a=t("Geolocation",{web:()=>r(()=>import("./web-BuI-snkz.js"),__vite__mapDeps([0,1,2,3,4,5])).then(o=>new o.GeolocationWeb)});e();export{a as Geolocation};
-//# sourceMappingURL=index-CWWlRcJT.js.map
