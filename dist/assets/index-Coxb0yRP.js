8587bad5d536dc78958a81779cf64b5bad0f7126
Brian Bakari <brianbakari22@gmail.com>
2026-07-13 20:06:35 +0400

    deploy: instant delete, layout fix, security hardening

--- dist/assets/index-Coxb0yRP.js
+++
@@ -1,3 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CaiPVOUp.js","assets/index-CzlAKFgY.js","assets/vendor-TTj4DRuz.js","assets/supabase-CwrFKnKJ.js","assets/ui-VlKIP8q1.js","assets/index-C3tPJB2X.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-CzlAKFgY.js";import"./vendor-TTj4DRuz.js";import"./supabase-CwrFKnKJ.js";import"./ui-VlKIP8q1.js";const m=r("App",{web:()=>t(()=>import("./web-CaiPVOUp.js"),__vite__mapDeps([0,1,2,3,4,5])).then(p=>new p.AppWeb)});export{m as App};
-//# sourceMappingURL=index-Coxb0yRP.js.map
