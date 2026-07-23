2d5d9530a9c4d5ca3b131a476567efb0d1d30029
Brian Bakari <brianbakari22@gmail.com>
2026-07-23 16:45:43 +0400

    fix: AdminClaims realtime fixed, GO requests query fixed, vite chunk limit updated, Pesapal removed from DB

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
+    chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
           supabase: ['@supabase/supabase-js'],
           ui: ['react-hot-toast'],
+          capacitor: ['@capacitor/core', '@capacitor/filesystem', '@capacitor/browser'],
+          sentry: ['@sentry/react'],
         }
       }
     }
